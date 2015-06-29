2d8756885f130afe2eebd55e174a9d9a33e67d74
Andrés González <andres@simbiotica.es>
2015-06-29 19:29:05 +0200

    signals bg

--- app/scripts/views/introduction.js
+++ app/scripts/views/introduction.js
@@ -4,6 +4,9 @@ var IntroductionView = Backbone.View.extend({
   el: '.introduction',
 
   initialize: function() {
+    $(window).on('scroll', _.bind(this.doParalaxEfect, this));
+    this.bg = $('.bg');
+    this.doParalaxEfect()
     this.launchModal();
   },
 
@@ -16,6 +19,24 @@ var IntroductionView = Backbone.View.extend({
       $('#modal-intro').foundation('reveal', 'open');
       $video[0].play();
     });
+  },
+
+  getScroll: function() {
+    return window.pageYOffset;
+  },
+
+  doParalaxEfect: function() {
+    var scroll = this.getScroll();
+
+    var translateY = (scroll / .7).toFixed(0)
+
+    this.bg.css({
+      // '-webkit-transform': 'translate3d(0,' + translateY + 'px, 0)',
+      // '-moz-transform': 'translate3d(0,' + translateY + 'px, 0)',
+      // '-ms-transform': 'translate3d(0,' + translateY + 'px, 0)',
+      // '-o-transform': 'translate3d(0,' + translateY + 'px, 0)',
+      'transform' :  'translateY(' + translateY + 'px)'
+     });
   }
 
 });
