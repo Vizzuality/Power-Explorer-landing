dcde2829275c6a5dcf00dbe69bc50747f3ba4ca5
David Inga <david.inga@vizzuality.com>
2016-12-04 09:03:16 +0100

    better styles and responsive mode

--- app/index.js
+++ app/index.js
@@ -1,2 +1,36 @@
 import 'foundation-sites/dist/css/foundation-flex.css';
+import 'slick-carousel/slick/slick.css';
 import './styles/index.scss';
+
+import $ from 'jquery';
+import slick from 'slick-carousel/slick/slick';
+
+function onReady() {
+  const $page = $('html, body');
+  const $scrollButton = $('#scrollButton');
+  const $navigation = $('#navigation');
+  const windowHeight = window.innerHeight;
+
+  $scrollButton.on('click', () => {
+    $page.animate({ scrollTop: windowHeight }, 116);
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset <= windowHeight) {
+      const opacityValue = (windowHeight - window.pageYOffset) / windowHeight;
+      $scrollButton.css('opacity', opacityValue);
+    }
+    if (window.pageYOffset > windowHeight) {
+      $navigation.removeClass('-hide');
+    } else {
+      $navigation.addClass('-hide');
+    }
+  });
+}
+
+function onLoad() {
+  $('.m-slider').slick();
+}
+
+document.addEventListener('DOMContentLoaded', onReady);
+window.addEventListener('load', onLoad);
