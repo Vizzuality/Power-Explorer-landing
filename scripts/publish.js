07398be8bc15720f71161345cc9989941f31d20c
Logan Byers <logan.byers@wri.org>
2018-05-03 13:22:38 -0700

    Update publish.js to maintain domain after push
    Update scripts/publish.js to ensure the CNAME file contains
    powerexplorer.org when updating the gh-pages branch.
    
    This fixes a bug where after running the publish script the
    custom domain would be reset.

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,6 +1,12 @@
 const ghpages = require('gh-pages');
 const path = require('path');
+const fs = require('fs');
 
+fs.writeFile(path.join(__dirname, '../dist', 'CNAME'), "powerexplorer.org", (err) => {
+  if (err) {
+    console.error('Error: %s', err);
+  }
+});
 ghpages.publish(path.join(__dirname, '../dist'), (err) => {
   if (err) {
     console.error('Error: %s', err);
